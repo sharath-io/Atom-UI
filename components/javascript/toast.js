234a704b168aac5440599043850280d2466e9e43
Sharath <sharath.esr@gmail.com>
2022-04-06 12:48:59 +0530

    feat: added setTimeout to close toast

--- components/javascript/toast.js
+++ components/javascript/toast.js
@@ -6,18 +6,21 @@ const bottomToast = document.querySelector('#bottom-toast-id');
 const bottomToastContent = document.querySelector('#bottom-content');
 const bottomToastClose = document.querySelector('#bottom-toast-close');
 
-const openTopToast = () => topToastContent.style.display = 'block';
-
+const openTopToast = () => {
+    topToastContent.style.display = 'block';
+    setTimeout(() => closeTopToast(), 3000);
+}
 const closeTopToast = () => topToastContent.style.display = 'none';
 
 topToast.addEventListener("click", openTopToast);
 topToastClose.addEventListener("click", closeTopToast);
 
 
-
-const openBottomToast = () => bottomToastContent.style.display = 'block';
+const openBottomToast = () => {
+    bottomToastContent.style.display = 'block';
+    setTimeout(() => closeBottomToast(), 3000);
+}
 const closeBottomToast = () => bottomToastContent.style.display = 'none';
-
 
 bottomToast.addEventListener("click", openBottomToast);
 bottomToastClose.addEventListener("click", closeBottomToast);
