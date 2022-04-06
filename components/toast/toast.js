c30dc23cf998f937fbad150895d3a1991ff90820
Sharath <sharath.esr@gmail.com>
2022-04-06 17:03:20 +0530

    refactor:updated variables for toast display style

--- components/toast/toast.js
+++ components/toast/toast.js
@@ -6,21 +6,24 @@ const bottomToast = document.querySelector('#bottom-toast-id');
 const bottomToastContent = document.querySelector('#bottom-content');
 const bottomToastClose = document.querySelector('#bottom-toast-close');
 
+const show = 'block';
+const hide = 'none';
+
 const openTopToast = () => {
-    topToastContent.style.display = 'block';
+    topToastContent.style.display = show;
     setTimeout(() => closeTopToast(), 3000);
 }
-const closeTopToast = () => topToastContent.style.display = 'none';
+const closeTopToast = () => topToastContent.style.display = hide;
 
 topToast.addEventListener("click", openTopToast);
 topToastClose.addEventListener("click", closeTopToast);
 
 
 const openBottomToast = () => {
-    bottomToastContent.style.display = 'block';
+    bottomToastContent.style.display = show;
     setTimeout(() => closeBottomToast(), 3000);
 }
-const closeBottomToast = () => bottomToastContent.style.display = 'none';
+const closeBottomToast = () => bottomToastContent.style.display = hide;
 
 bottomToast.addEventListener("click", openBottomToast);
 bottomToastClose.addEventListener("click", closeBottomToast);
